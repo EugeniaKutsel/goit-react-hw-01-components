847031d743ed32ca28e10e2f8a162c3f5b5e98fe
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-10-24 23:40:07 +0300

    Update task_2 (statistics)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
 import Profile from "./Profile/Profile";
-import user from "./Profile/user.json"
+import user from "./Profile/user.json";
+
+import Statistics from "./Statistics/Statistics";
+import data from "./Statistics/data.json";
 
 const App = () => {
   return (
-    <Profile
+    <div className="app">
+      <Profile
       username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-    />
-  )
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+    </div>
+  );
 }
 
 export default App;
