93e5c2bd7640493493c9a809ec1d2266e5e3d612
Eugenia Kutsel <jenna.kutsel@gmail.com>
2022-10-24 23:30:28 +0300

    Update task_1 (Profile)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
+import Profile from "./Profile/Profile";
+import user from "./Profile/user.json"
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <Profile
+      username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+    />
+  )
+}
+
+export default App;
